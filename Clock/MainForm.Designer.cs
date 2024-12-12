e8364f96cb2aafe5eaf75ab9e2d968c1c529d7e9
yumin <yumin.dima@gmail.com>
2024-12-12 22:08:07 +0300

    Add project files.

---
+++ Clock/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Clock
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.labelTime = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.cbShowDate = new System.Windows.Forms.CheckBox();
+            this.btnHideControls = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            resources.ApplyResources(this.labelTime, "labelTime");
+            this.labelTime.Name = "labelTime";
+            this.labelTime.DoubleClick += new System.EventHandler(this.labelTime_DoubleClick);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // cbShowDate
+            // 
+            resources.ApplyResources(this.cbShowDate, "cbShowDate");
+            this.cbShowDate.Name = "cbShowDate";
+            this.cbShowDate.UseVisualStyleBackColor = true;
+            this.cbShowDate.CheckedChanged += new System.EventHandler(this.CheckBoxShowDate_CheckedChanged);
+            // 
+            // btnHideControls
+            // 
+            resources.ApplyResources(this.btnHideControls, "btnHideControls");
+            this.btnHideControls.Name = "btnHideControls";
+            this.btnHideControls.UseVisualStyleBackColor = true;
+            this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnHideControls);
+            this.Controls.Add(this.cbShowDate);
+            this.Controls.Add(this.labelTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.CheckBox cbShowDate;
+        private System.Windows.Forms.Button btnHideControls;
+    }
+}
+
---
+++ Clock/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.SymbolStore;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt", System.Globalization.CultureInfo.InvariantCulture);
+            if (cbShowDate.Checked)
+            {
+                labelTime.Text += "\n";
+                labelTime.Text += DateTime.Now.ToString("dd.MM.yy");
+            }
+        }
+
+        private void CheckBoxShowDate_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnHideControls_Click(object sender, EventArgs e)
+        {
+            cbShowDate.Visible  = false;
+            btnHideControls.Visible = false;
+            this.TransparencyKey = this.BackColor;
+            this.FormBorderStyle = FormBorderStyle.None;
+            labelTime.BackColor = Color.AliceBlue;
+            this.ShowInTaskbar = false;
+        }
+
+        private void labelTime_DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show
+                (
+                "Вы два раза щелкнули мышью по времени и теперь Вы управляете временем!",
+                "Info",
+                MessageBoxButtons.OK, 
+                MessageBoxIcon.Information
+                );
+            MessageBox.Show(
+                "Шучу, вжух и окно увеличилось",
+                "Info",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+                );
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            this.TransparencyKey = Color.Transparent;
+            labelTime.BackColor = default;
+            cbShowDate.Visible = true;
+            btnHideControls.Visible = true;
+            this.ShowInTaskbar = true;
+        }
+    }
+}
